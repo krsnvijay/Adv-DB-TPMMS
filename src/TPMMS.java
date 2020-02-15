9dd8887185ce5756416102cd5c510664ad570d7b
Siddhant Bansal <si_bansa@live.concordia.ca>
2020-02-15 17:51:04 -0500

    i don't even know anymore

--- src/TPMMS.java
+++ src/TPMMS.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.Arrays;
 
 public class TPMMS {
   private static BufferedReader reader;
@@ -11,10 +10,12 @@ public class TPMMS {
 
   private final int STARTBYTE = 0;
   private final int ENDBYTE = System.getProperty("os.name").toLowerCase().contains("win") ? 101 : 100;
+  private int currentPass = 0;
+  private int tuplesWrittenInPass = 0;
 
-  private File tempFile = new File("Employee-Generator/temp-file.txt");
-  private File finalFile = new File("Employee-Generator/sorted.txt");
-  
+  private File tempFile = new File("Employee-Generator/sorted");
+  private File finalFile = new File("Employee-Generator/sorted");
+
   private int numOfTuplesPerPage;
   private int totalNumOfPages;
   private int numOfTuplesInLastBlock;
@@ -24,7 +25,7 @@ public class TPMMS {
     try {
       runPhase1();
       runPhase2();
-    } catch(IOException e) {
+    } catch (IOException e) {
       e.printStackTrace();
     }
   }
@@ -38,20 +39,20 @@ public class TPMMS {
   }
 
   private void performWrite(int recordCounter, char[][] lines) throws IOException {
-    if(recordCounter == 0) {
+    if (recordCounter == 0) {
       writer.append(String.copyValueOf(lines[0]));
     }
-    for(int i=0; i<recordCounter; i++) {
+    for (int i = 0; i < recordCounter; i++) {
       writer.append(String.copyValueOf(lines[i]));
     }
   }
 
   private boolean shouldSwap(char[] record1, char[] record2) {
-    if(Integer.parseInt(new String(record1,0,8)) <
-            Integer.parseInt(new String(record2, 0, 8))) return true;
-
-    else if(Integer.parseInt(new String(record1,0,8)) ==
-            Integer.parseInt(new String(record2, 0, 8))) {
+    if (Integer.parseInt(new String(record1, 0, 8)) <
+        Integer.parseInt(new String(record2, 0, 8))) {
+      return true;
+    } else if (Integer.parseInt(new String(record1, 0, 8)) ==
+        Integer.parseInt(new String(record2, 0, 8))) {
       RecordComparator rC = new RecordComparator();
       return rC.compare(String.valueOf(record1), String.valueOf(record2)) < 0;
     }
@@ -62,7 +63,7 @@ public class TPMMS {
     char[] pivot = arr[high];
     int i = (low - 1);
     for (int j = low; j <= high - 1; j++) {
-    if (shouldSwap(arr[j],pivot)) {
+      if (shouldSwap(arr[j], pivot)) {
         i++;
         char[] temp = arr[i];
         arr[i] = arr[j];
@@ -88,18 +89,18 @@ public class TPMMS {
     long freeMemoryEstimationPhase1 = (long) Math.ceil(MemoryHandler.getInstance().getFreeMemory());
 
     totalNumOfPages = (int) Math.ceil((numOfRecords * ENDBYTE) / (double) freeMemoryEstimationPhase1);
-    numOfTuplesPerPage = (numOfRecords)/totalNumOfPages;
+    numOfTuplesPerPage = (numOfRecords) / totalNumOfPages;
 
     // safeguard
-    if(numOfTuplesPerPage > 12500) {
+    if (numOfTuplesPerPage > 12500) {
       numOfTuplesPerPage = 5000;
-      totalNumOfPages = numOfRecords/numOfTuplesPerPage;
+      totalNumOfPages = numOfRecords / numOfTuplesPerPage;
     }
   }
 
   private void sortFile(String filePath) throws IOException {
     reader = new BufferedReader(new FileReader(filePath));
-    writer = new BufferedWriter(new FileWriter(tempFile));
+    writer = new BufferedWriter(new FileWriter(tempFile + "_0.txt"));
 
     handlePageCalculations();
 
@@ -111,33 +112,39 @@ public class TPMMS {
       System.arraycopy(line, 0, lines[recordCounter], 0, ENDBYTE);
       recordCounter++;
       if (recordCounter == numOfTuplesPerPage) {
-        recordSort(lines,0, recordCounter-1);
-        performWrite(recordCounter,lines);
+        recordSort(lines, 0, recordCounter - 1);
+        performWrite(recordCounter, lines);
         recordCounter = 0;
       }
     }
 
     numOfTuplesInLastBlock = recordCounter;
 
-    if(recordCounter > 0) {
-      recordSort(lines,0, recordCounter-1);
-      performWrite(recordCounter,lines);
+    if (recordCounter > 0) {
+      recordSort(lines, 0, recordCounter - 1);
+      performWrite(recordCounter, lines);
     }
 
     reader.close();
     writer.close();
-    System.out.printf("Blocks -- %d | Tuples/block -- %d \n",totalNumOfPages,numOfTuplesPerPage);
+    System.out.printf("Blocks -- %d | Tuples/block -- %d \n", totalNumOfPages, numOfTuplesPerPage);
+  }
+
+  public int getNumberOfJoins(int currentPass) {
+    long freeMem = MemoryHandler.getInstance().getFreeMemory();
+    int fileSize = numOfRecords * ENDBYTE;
+    int numOfInputBuffers = (int) Math.floor(freeMem / (numOfTuplesPerPage * ENDBYTE)) - 1;
+    return (int) Math.ceil(totalNumOfPages/Math.pow(numOfInputBuffers, currentPass));
   }
 
   public void mergeKWay(File tempFilePath) throws IOException {
 
-    RandomAccessFile raf = new RandomAccessFile(tempFilePath, "r");
-    writer = new BufferedWriter(new FileWriter(finalFile));
+    RandomAccessFile raf;
 
     long freeMem = MemoryHandler.getInstance().getFreeMemory();
-    int numOfInputBuffers = (int) Math.floor(freeMem/(numOfTuplesPerPage*ENDBYTE)) - 1;
-    int fileSize = numOfRecords*ENDBYTE;
-    int totalPasses = (int) Math.ceil(Math.log(fileSize/freeMem) / Math.log(numOfInputBuffers));
+    int numOfInputBuffers = (int) Math.floor(freeMem / (numOfTuplesPerPage * ENDBYTE)) - 1;
+    int fileSize = numOfRecords * ENDBYTE;
+    int totalPasses = (int) Math.ceil(Math.log(fileSize / freeMem) / Math.log(numOfInputBuffers));
     int chunkSize = numOfTuplesPerPage;
     short outIndex = 0;
     /*
@@ -149,98 +156,149 @@ public class TPMMS {
       3rd Pass = 80000
       ....
      */
-    for(int pass=0; pass<totalPasses; pass++) {
+    for (int pass = 0; pass < totalPasses; pass++) {
+      this.currentPass = pass;
+      raf = new RandomAccessFile(tempFile + "_" + (pass) + ".txt", "r");
+      writer = new BufferedWriter(new FileWriter(finalFile + "_" + (pass + 1) + ".txt"));
       byte[][] outputBuffer = new byte[numOfTuplesPerPage][ENDBYTE];
       int[] runPointers = new int[numOfInputBuffers];
       byte[][][] buffer = new byte[numOfInputBuffers][numOfTuplesPerPage][ENDBYTE];
 
       // initial read alone
+//      try {
       for (int i = 0; i < numOfInputBuffers; i++) {
-        int start = i*numOfTuplesPerPage*ENDBYTE;
-        int offset = runPointers[i]*ENDBYTE;
-        int seekVal = start + offset;
-        raf.seek(seekVal);
-        for(int j=0;j<numOfTuplesPerPage;j++) {
-          raf.readFully(buffer[i][j]);
-        }
-      }
-      // block-wise merging
-      while(!exhaustedBlocks(buffer,runPointers,chunkSize,numOfInputBuffers)) {
-        int idxBlock = indexOfBlockWithMinTuple(buffer,runPointers, 0, -1);
-        byte[] minTupleBuffer = buffer[idxBlock][runPointers[idxBlock]];
-        runPointers[idxBlock] += 1;
-        outputBuffer[outIndex] = minTupleBuffer;
-        outIndex++;
-
-        if(outIndex == numOfTuplesPerPage - 1) {
-          for(int i=0; i<outputBuffer.length; i++) {
-            writer.append(new String(outputBuffer[i]));
+          int seekVal = i * chunkSize * ENDBYTE;
+          raf.seek(seekVal);
+          if((seekVal + numOfTuplesPerPage*ENDBYTE) > fileSize) {
+            int tempSize = (fileSize - seekVal)/ENDBYTE;
+            //byte[][][]
+            //raf.readFully();
           }
-          outIndex = 0;
+          for (int j = 0; j < numOfTuplesPerPage; j++) {
+            raf.readFully(buffer[i][j]);
+          }
         }
-      }
-      // TO FIX - OP buffer seems to have corrupt last index values
-      if(outIndex > 0) {
-        for(int i=0; i<outIndex; i++) {
+        // block-wise merging
+        while (!exhaustedBlocks(buffer, runPointers, chunkSize, numOfInputBuffers)) {
+          int idxBlock = indexOfBlockWithMinTuple(buffer, runPointers, 0, -1);
+          byte[] minTupleBuffer = buffer[idxBlock][runPointers[idxBlock]];
+          runPointers[idxBlock] += 1;
+          outputBuffer[outIndex] = minTupleBuffer;
+          outIndex++;
+
+          if (outIndex == numOfTuplesPerPage) {
+            for (int i = 0; i < outputBuffer.length; i++) {
+              writer.append(new String(outputBuffer[i]));
+            }
+            outIndex = 0;
+          }
+
+//          if (outIndex > 0) {
+//            for (int i = 0; i < outIndex; i++) {
+//              writer.append(new String(outputBuffer[i]));
+//            }
+//          }
+        }
+//      }
+//      catch(EOFException e) {
+//        System.out.println("Hit EOF");
+//        if (outIndex > 0) {
+//          for (int i = 0; i < outIndex; i++) {
+//            writer.append(new String(outputBuffer[i]));
+//          }
+//        }
+//      }
+
+      if (outIndex > 0) {
+        for (int i = 0; i < outIndex; i++) {
           writer.append(new String(outputBuffer[i]));
         }
       }
       // update disk chunk-size
       chunkSize *= numOfInputBuffers;
+      raf.close();
     }
+
+    writer.close();
   }
 
   private int indexOfBlockWithMinTuple(byte[][][] buffer, int[] runPointers, int currIndex, int minIndex) {
-    if(currIndex == buffer.length) {
+    if (currIndex == buffer.length) {
       return minIndex;
     }
-    if(runPointers[currIndex] >= numOfTuplesPerPage) {
-      return indexOfBlockWithMinTuple(buffer, runPointers, currIndex+1,  minIndex);
+    if (runPointers[currIndex] >= numOfTuplesPerPage) {
+      return indexOfBlockWithMinTuple(buffer, runPointers, currIndex + 1, minIndex);
     }
-    if(minIndex == -1) {
-      return indexOfBlockWithMinTuple(buffer, runPointers, currIndex+1, currIndex);
+    if (minIndex == -1) {
+      return indexOfBlockWithMinTuple(buffer, runPointers, currIndex + 1, currIndex);
     }
 
     int empID1 = Integer.parseInt(new String(buffer[minIndex][runPointers[minIndex]], 0, 8));
     int empID2 = Integer.parseInt(new String(buffer[currIndex][runPointers[currIndex]], 0, 8));
-    if(empID1 > empID2) {
+    if (empID1 > empID2) {
       minIndex = currIndex;
-    }
-    else if(empID1 == empID2) {
+    } else if (empID1 == empID2) {
       RecordComparator rC = new RecordComparator();
       boolean res = rC.compare(new String(buffer[minIndex][runPointers[minIndex]]),
-              new String(buffer[currIndex][runPointers[currIndex]])) < 0;
-      minIndex = res? currIndex: minIndex;
+          new String(buffer[currIndex][runPointers[currIndex]])) < 0;
+      minIndex = res ? currIndex : minIndex;
     }
 
-    return indexOfBlockWithMinTuple(buffer, runPointers, currIndex+1, minIndex);
+    return indexOfBlockWithMinTuple(buffer, runPointers, currIndex + 1, minIndex);
   }
 
   private boolean exhaustedBlocks(byte[][][] buffer, int[] runsPerPagePointers, int maxTuples, int numOfInputBuffers) {
+
+    // is the chunk (15000) exhausted?
     int sum = 0, counter = 0;
-    for(int pointer:runsPerPagePointers){
+    for (int pointer : runsPerPagePointers) {
       sum += pointer;
     }
-    if(sum == maxTuples*numOfInputBuffers) return true;
-    for(int pointer:runsPerPagePointers) {
-      if (pointer == numOfTuplesPerPage && maxTuples > pointer) {
-        readNextChunkInBuffer(buffer, pointer, counter, maxTuples);
-        runsPerPagePointers[counter] = 0;
+    if (sum == maxTuples * numOfInputBuffers) {
+      readNextBlocksInFile(filePath, buffer, numOfTuplesPerPage, numOfInputBuffers, runsPerPagePointers);
+      for(int i=0; i<runsPerPagePointers.length; i++) {
+        runsPerPagePointers[i] = 0;
       }
-      counter++;
+      return true;
     }
+
+    // are the buffers exhausted?
+    for (int i = 0; i < runsPerPagePointers.length; i++) {
+      int pointer = runsPerPagePointers[i];
+      if (pointer == numOfTuplesPerPage && maxTuples > pointer) {
+        readNextChunkInBuffer(buffer, pointer, i, maxTuples);
+      }
+    }
+
+
+
     return false;
+  }
+
+  private void readNextBlocksInFile(String filePath, byte[][][] buffer, int numOfTuplesPerPage, int numOfInputBuffers, int[] runsPerPagePointers) {
+    try {
+      RandomAccessFile raf = new RandomAccessFile(String.format("%s_%d.txt", finalFile, currentPass), "r");
+      int startOfNextBlock = runsPerPagePointers[numOfInputBuffers -1];
+
+      for (int i = 0; i < numOfInputBuffers; i++) {
+        int start = (i * numOfTuplesPerPage + startOfNextBlock) * ENDBYTE; // verify this calculation
+        raf.seek(start);
+        for (int j = 0; j < numOfTuplesPerPage; j++)
+          raf.readFully(buffer[i][j]);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
   private void readNextChunkInBuffer(byte[][][] buffer, int currRunPointer, int counter, int chunkSize) {
     try {
-      RandomAccessFile raf = new RandomAccessFile(finalFile, "r");
+      RandomAccessFile raf = new RandomAccessFile(String.format("%s_%d.txt", finalFile, currentPass), "r");
       int start = (counter * numOfTuplesPerPage + currRunPointer) * ENDBYTE; // verify this calculation
       raf.seek(start);
       for (int j = 0; j < numOfTuplesPerPage; j++)
         raf.readFully(buffer[counter][j]);
-    }
-    catch(Exception e) {
+    } catch (Exception e) {
       e.printStackTrace();
     }
   }
